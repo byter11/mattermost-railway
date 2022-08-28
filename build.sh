ce7c3b934d0958eec15e603140f26082b2a1a2a4
Mohsin Shaikh <55991791+byter11@users.noreply.github.com>
2022-08-28 15:14:56 +0500

    config update

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ mkdir mattermost/
 echo "-----> Retrieving mattermost tar from ${MATTERMOST_DOWNLOAD_URI}"
 curl -s -L "${MATTERMOST_DOWNLOAD_URI}" | tar -zxf - --strip-components=1 -C ./mattermost
 
+mv ./config.json ./mattermost/config/
 mkdir data/
