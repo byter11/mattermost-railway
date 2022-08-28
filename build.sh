8c3cf56fdbf15173dd6debe2fc0c86d6a5ac6053
Mohsin Shaikh <55991791+byter11@users.noreply.github.com>
2022-08-28 14:42:57 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -e
 mkdir mattermost/
 echo "-----> Retrieving mattermost tar from ${MATTERMOST_DOWNLOAD_URI}"
 curl -s -L "${MATTERMOST_DOWNLOAD_URI}" | tar -zxf - --strip-components=1 -C ./mattermost
-mkdir data/
-chmod +x ./run.sh
+
+export MM_SERVICESETTINGS_SITEURL=${RAILWAY_STATIC_URL:=""}
+export MM_SERVICESETTINGS_LISTENADDRESS="0.0.0.0:${PORT}"
