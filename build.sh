eb0e35a737c5f2e0d60ba20f78c4d60f0b6e4de8
Mohsin Shaikh <55991791+byter11@users.noreply.github.com>
2022-08-28 14:22:09 +0500

    fix permission error

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ BUILD_DIR=/bin
 echo "-----> Retrieving mattermost tar from ${MATTERMOST_DOWNLOAD_URI}"
 curl -s -L "${MATTERMOST_DOWNLOAD_URI}" | tar -zxf - --strip-components=1 -C ${BUILD_DIR}
 mkdir data/
+chmod +x ./bin/release
