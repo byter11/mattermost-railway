f1e3ec6b4371ee1bcd1473809f6f8cf63e5c6b54
Mohsin Shaikh <55991791+byter11@users.noreply.github.com>
2022-08-28 15:21:26 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ mkdir mattermost/
 echo "-----> Retrieving mattermost tar from ${MATTERMOST_DOWNLOAD_URI}"
 curl -s -L "${MATTERMOST_DOWNLOAD_URI}" | tar -zxf - --strip-components=1 -C ./mattermost
 
-mv ./config.json ./mattermost/config/
+export MM_SERVICESETTINGS_LISTENADDRESS="0.0.0.0:${PORT}"
+export MM_SERVICESETTINGS_SITEURL="${RAILWAY_STATIC_URL}"
+export MM_SQLSETTINGS_DATASOURCE=${DATABASE_URL}
+
 mkdir data/
