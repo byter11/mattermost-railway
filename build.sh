b863247fff15ac9b0dbdc06f429e589e2f9f90fc
Mohsin Shaikh <55991791+byter11@users.noreply.github.com>
2022-08-28 13:39:32 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BUILD_DIR=/bin/mattermost
+BUILD_DIR=/bin
 
 echo "-----> Retrieving mattermost tar from ${MATTERMOST_DOWNLOAD_URI}"
 curl -s -L "${MATTERMOST_DOWNLOAD_URI}" | tar -zxf - --strip-components=1 -C ${BUILD_DIR}
